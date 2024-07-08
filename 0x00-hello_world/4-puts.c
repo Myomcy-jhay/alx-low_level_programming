8b120dc655c579f15c57fa5e8e5f055027383944
Myomcy-jhay <144353139+Myomcy-jhay@users.noreply.github.com>
2024-07-08 17:16:47 +0100

    Add files via upload

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle
- * Return: 0 if exited properly, non-zero otherwise
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-
 /**
- * main - prints the phrase with proper grammar,
- * but the outcome is a piece of art,
- * Return: 0 if exited properly, non-zero otherwise
+ * main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
